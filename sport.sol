c7b9d03c581526a4c6d75f6d5c144ccf55c70198
KennyIssak <kennyissak@gmail.com>
2022-06-02 18:03:32 +0800

    feat: ERC721 and interaction with ERC20

--- sport.sol
+++ sport.sol
@@ -2,18 +2,32 @@ pragma solidity ^0.4.16;
 
 import "./score.sol";
 
+interface Medal {
+    function awardItem(address player, string memory tokenURI) public returns (uint256);
+}
+
 contract Sport is Score(100000000) {
+
     uint constant walkingC = 405;
     uint constant runningC = 677;
     uint constant bikingC = 415;
     uint constant swimmingC = 550;
     uint constant fitnessTrainingC = 660;
 
+    Medal medalContract;
+
+    function Sport(address medal) {
+        medalContract = Medal(medal);
+    }
 
     function _scoreDistribution(uint calories) private {
         _transfer(this, msg.sender, 10 * (calories / 100));
     }
 
+    function exchange() external payable {
+        transfer(this, 1000);
+        medalContract.awardItem(msg.sender, "");
+    }
 
     function doWalking(uint time) external {
         uint calories = walkingC * time;
